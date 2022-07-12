5279f3f141c42d6da62c188dfb5eab2b6e75dbd6
Ushiris <35918080+Ushiris@users.noreply.github.com>
2022-07-12 18:40:27 +0900

    fix interface

--- data/interface.js
+++ data/interface.js
@@ -3,3 +3,7 @@ const fs = require("fs");
 exports.isPremiumGuild = function(guildID){
     return JSON.parse(fs.readFileSync("./data/premiumUser.json")).guildList.includes(guildID);
 }
+
+exports.getPublicIP = function(){
+    return JSON.parse(fs.readFileSync("./data/network.json")).ip;
+}
